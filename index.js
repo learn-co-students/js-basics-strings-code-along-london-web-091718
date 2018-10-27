b458a53ee5e774168456d348eecb7b8e72025aca
ssong-eu <sangyoon.song@gmail.com>
2018-10-27 17:49:11 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code in this file!
+let currentUser = "Sang";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`; // or currentUser.substrig(0,1) or currentUser.charAt(0)
+
+
+
